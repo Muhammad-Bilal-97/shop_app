d1a013fc442123afed28b2794fd25b98e3cc8463
Muhammad Bilal <bilalroze94@gmail.com>
2022-11-23 19:38:16 +0500

    cart screen

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
+import 'providers/cart.dart';
+import 'screens/cart_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            // brightness: Brightness.dark,
+            primarySwatch: Colors.purple,
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+              secondary: Colors.deepOrange, // Your accent color
+            ),
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
+          }),
+    );
+  }
+}
